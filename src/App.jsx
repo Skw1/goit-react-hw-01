c7c74ebcc907fc2e5167837606aa815033e8f07e
Andrii Doluda <andrewdoluda@gmail.com>
2024-02-20 16:19:03 +0100

    First task Profile

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// App
+import Profile from './components/Profile/Profile.jsx'; 
+import userData from './userData.json'; 
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App;
